425e8a042487603081b2fe117a1c20fa963adffb
aster <137767097+aster-void@users.noreply.github.com>
2024-10-03 00:33:22 +0900

    fix errors not showing up properly

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,25 +1,63 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
-  import * as ws from "$lib/api/workspace";
+  import * as workspace from "$lib/api/workspace";
+  import type { Workspace } from "$lib/types";
+  import { onMount } from "svelte";
+
+  let joined: Workspace[];
+  let joinedErr: Error;
+
+  let publics: Workspace[];
+  let publicsErr: Error;
+
+  onMount(() => {
+    workspace
+      .joined()
+      .then((j) => (joined = j))
+      .catch((err) => (joinedErr = err));
+
+    workspace
+      .publics()
+      .then((p) => (publics = p))
+      .catch((err) => (publicsErr = err));
+  });
 </script>
 
 <h1>Just a Chat App</h1>
 
-{#await ws.joined()}
+<h2>Joined Workspaces</h2>
+{#if joinedErr}
+  Error: {joinedErr}
+{:else if !joined}
   Loading...
-{:then joined}
-  {#each joined as workspace}
-    - {workspace.name}
-  {/each}
-{/await}
+{:else}
+  <ul>
+    {#each joined as workspace}
+      <li>
+        {workspace.name}
+        <button on:click={() => goto(`/workspace/${workspace.id}`)}>Go</button>
+      </li>
+    {/each}
+  </ul>
+{/if}
 
 <button on:click={() => goto("/workspace/create")}> Create New </button>
 <hr />
-{#await ws.publics()}
+
+<h2>Public Workspaces</h2>
+{#if publicsErr}
+  Error: {publicsErr}
+{:else if !publics}
   loading...
-{:then pub}
-  {#each pub as workspace}
-    - {workspace.name}
-    <button on:click={() => alert(`Joined ${workspace.name}`)}> Join </button>
-  {/each}
-{/await}
+{:else}
+  <ul>
+    {#each publics as workspace}
+      <li>
+        {workspace.name}
+        <button on:click={() => alert(`Joined ${workspace.name}`)}>
+          Join
+        </button>
+      </li>
+    {/each}
+  </ul>
+{/if}
--- web/src/routes/workspace/[id=number]/+page.svelte
+++ web/src/routes/workspace/[id=number]/+page.svelte
@@ -1,18 +1,30 @@
 <script lang="ts">
   import * as ws from "$lib/api/workspace";
   import { page } from "$app/stores";
-  const promise = ws.get(parseInt($page.params.id));
-  (async () => console.log(await promise))();
+  import { onMount } from "svelte";
+  import type { Workspace } from "$lib/types";
+
+  let workspace: Workspace;
+  let err: Error;
+
+  onMount(() => {
+    ws.get(parseInt($page.params.id))
+      .then((res) => {
+        workspace = res;
+      })
+      .catch((e) => {
+        err = e;
+      });
+  });
 </script>
 
 <h2>Workspace</h2>
-{#await promise}
+{#if err}
+  <p>Something went wrong: {err.message}</p>
+{:else if !workspace}
   Loading...
-{:then workspace}
+{:else}
   <p>
     You are in workspace {workspace.name}
   </p>
-{:catch e}
-  Something went wrong...
-  {e}
-{/await}
+{/if}
