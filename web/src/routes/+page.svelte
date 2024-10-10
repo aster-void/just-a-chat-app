d974efbcb273d3e4d98fcbc107d8738291a36b4e
aster <137767097+aster-void@users.noreply.github.com>
2024-10-11 00:50:14 +0900

    view: init tailwind + daisyui

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-  import * as ws from "$lib/api/workspace";
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
 </script>
 
-<h1>Just a Chat App</h1>
-
-<h2>Joined Workspaces</h2>
-<ul>
-  {#each data.joined as workspace}
-    <li>
-      {workspace.name}
-      <button on:click={() => goto(`/workspace/${workspace.id}`)}>Go</button>
-    </li>
-  {/each}
-</ul>
-<button on:click={() => goto("/workspace/create")}> Create New </button>
-<hr />
-
-<h2>Public Workspaces</h2>
-<ul>
-  {#each data.publics as workspace}
-    <li>
-      {workspace.name}
-      <button on:click={() => ws.join(fetch, workspace.id)}> Join </button>
-    </li>
-  {/each}
-</ul>
+<header>
+  <div>
+    <button class="btn btn-primary">Log In</button>
+  </div>
+</header>
+<article>
+  <h1>Just A Chat App</h1>
+</article>
---
+++ web/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+ content: [],
+ content: ['./src/**/*.{html,svelte,js,ts}'],
+ theme: {
+   extend: {},
+ },
+ plugins: [],
+ plugins: [require('daisyui')],
+}
