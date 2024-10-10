307f71c828a72de4f231f3e921e1a35c13070f36
aster <137767097+aster-void@users.noreply.github.com>
2024-10-11 01:57:17 +0900

    daisyUI is great

---
+++ web/src/components/NavBar.svelte
@@ -0,0 +1,19 @@
+<header>
+  <div class="navbar bg-base-60 border-10 border-base-500">
+    <div class="navbar-start">
+      <a href="/" class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="navbar-end">
+      <slot />
+    </div>
+  </div>
+</header>
+
+<style>
+  header {
+    position: sticky;
+    height: 10vh;
+    top: 0;
+    backdrop-filter: blur(30px);
+  }
+</style>
--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,11 +1,26 @@
 <script lang="ts">
+  import NavBar from "../components/NavBar.svelte";
 </script>
 
-<header>
-  <div>
-    <button class="btn btn-primary">Log In</button>
-  </div>
-</header>
-<article>
-  <h1>Just A Chat App</h1>
-</article>
+<main>
+  <NavBar>
+    <button class="btn btn-ghost">Log In</button>
+    <button class="btn btn-primary">Sign Up</button>
+  </NavBar>
+  <article>
+    <h1>Just A Chat App</h1>
+  </article>
+</main>
+
+<style>
+  button {
+    margin: 16px;
+  }
+  h1 {
+    font-size: 64px;
+    text-align: center;
+  }
+  main {
+    min-height: 200vh;
+  }
+</style>
